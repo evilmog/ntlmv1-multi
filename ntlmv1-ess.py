64197eff82ce896db7a56eb94d3dd7312a4ff8e6
EvilMog <evilmog@xfr-cracken-1.cambridge.ibm.com>
2018-04-30 15:48:24 -0400

    updated ntlmv1-ess.py

--- ntlmv1-ess.py
+++ ntlmv1-ess.py
@@ -23,18 +23,18 @@ print "Hostname: " + hashsplit[0]
 print "Username: " + hashsplit[2]
 print "Challenge: " + challenge
 print "Combined: " + combined
-print "CT1: " + ct1
-print "CT2: " + ct2
+#print "CT1: " + ct1
+#print "CT2: " + ct2
 print "F3: " + f3 + "\n"
 
 print "To Calculate final 4 characters of NTLM hash use:"
 print "./ct3_to_ntlm.bin " + ct3 + " " + challenge + " " + f3 + "\n"
 #./ct3_to_ntlm.bin 2e1e4bf33006ba41 cb8086049ec4736c 338d08f8e26de93300000000000000000000000000000000
 
-print "To crack with hashcat create a file with the following contents:"
-print ct1 + ":" + challenge
-print ct2 + ":" + challenge + "\n"
+#print "To crack with hashcat create a file with the following contents:"
+#print ct1 + ":" + challenge
+#print ct2 + ":" + challenge + "\n"
 
-print "To crack with hashcat:"
-print "./hashcat -m 14000 -a 3 -1 charsets/DES_full.charset --hex-charset hashes.txt ?1?1?1?1?1?1?1?1"
+#print "To crack with hashcat:"
+#print "./hashcat -m 14000 -a 3 -1 charsets/DES_full.charset --hex-charset hashes.txt ?1?1?1?1?1?1?1?1"
 
