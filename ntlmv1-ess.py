20978c558e17472707e22f11393836b6ca9c161c
cclauss <cclauss@bluewin.ch>
2018-05-02 13:37:11 +0200

    Modernize Python 2 code to get ready for Python 3

--- ntlmv1-ess.py
+++ ntlmv1-ess.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import hashlib,binascii
 import argparse
 import binascii
@@ -25,24 +26,24 @@ ct2 = ntresp[16:32]
 #Hashfield Split:
 #['u4-netntlm', '', 'kNS', '338d08f8e26de93300000000000000000000000000000000', '9526fb8c23a90751cdd619b6cea564742e1e4bf33006ba41', 'cb8086049ec4736c']
 
-print "Hashfield Split:"
-print str(hashsplit) + "\n"
+print("Hashfield Split:")
+print(str(hashsplit) + "\n")
 
-print "Hostname: " + hashsplit[2]
-print "Username: " + hashsplit[0]
-print "LM Response: " + lmresp
-print "NT Response: " + ntresp
-print "Client Challenge: " + clientchallenge
-print "SRV Challenge: " + srvchallenge + "\n"
+print("Hostname: " + hashsplit[2])
+print("Username: " + hashsplit[0])
+print("LM Response: " + lmresp)
+print("NT Response: " + ntresp)
+print("Client Challenge: " + clientchallenge)
+print("SRV Challenge: " + srvchallenge + "\n")
 
-print "To Calculate final 4 characters of NTLM hash use:"
-print "./ct3_to_ntlm.bin " + ct3 + " " + clientchallenge + " " + lmresp + "\n"
+print("To Calculate final 4 characters of NTLM hash use:")
+print("./ct3_to_ntlm.bin " + ct3 + " " + clientchallenge + " " + lmresp + "\n")
 #./ct3_to_ntlm.bin 2e1e4bf33006ba41 cb8086049ec4736c 338d08f8e26de93300000000000000000000000000000000
 
-print "To crack with hashcat create a file with the following contents:"
-print ct1 + ":" + srvchallenge
-print ct2 + ":" + srvchallenge + "\n"
+print("To crack with hashcat create a file with the following contents:")
+print(ct1 + ":" + srvchallenge)
+print(ct2 + ":" + srvchallenge + "\n")
 
-print "To crack with hashcat:"
-print "./hashcat -m 14000 -a 3 -1 charsets/DES_full.charset --hex-charset hashes.txt ?1?1?1?1?1?1?1?1"
+print("To crack with hashcat:")
+print("./hashcat -m 14000 -a 3 -1 charsets/DES_full.charset --hex-charset hashes.txt ?1?1?1?1?1?1?1?1")
 
