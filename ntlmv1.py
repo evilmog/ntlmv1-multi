4cc54b13cdd919d83a3a107877277972ffed4a54
evilmog <evilmog@ibm.com>
2019-12-10 09:12:58 -0700

    updates to make this python3 compat

--- ntlmv1.py
+++ ntlmv1.py
@@ -14,34 +14,34 @@ ct2 = ntresp[16:32]
 ct3 = ntresp[32:48]
 
 if lmresp[16:48] == "00000000000000000000000000000000":
-  print "Hash response is SSP, use ntlmv1-ssp.py --ssp instead"
+  print("Hash response is SSP, use ntlmv1-ssp.py --ssp instead")
   sys.exit()
 
-print "Hashfield Split:"
-print str(hashsplit) + "\n"
+print("Hashfield Split:")
+print(str(hashsplit) + "\n")
 
-print "Hostname: " + hashsplit[2]
-print "Username: " + hashsplit[0]
-print "Challenge: " + challenge
-print "LM Response: " + lmresp
-print "NT Response: " + ntresp
-print "CT1: " + ct1
-print "CT2: " + ct2
-print "CT3: " + ct3 + "\n"
+print("Hostname: " + hashsplit[2])
+print("Username: " + hashsplit[0])
+print("Challenge: " + challenge)
+print("LM Response: " + lmresp)
+print("NT Response: " + ntresp)
+print("CT1: " + ct1)
+print("CT2: " + ct2)
+print("CT3: " + ct3 + "\n")
 
-print "To Calculate final 4 characters of NTLM hash use:"
-print "./ct3_to_ntlm.bin " + ct3 + " " + challenge + "\n"
+print("To Calculate final 4 characters of NTLM hash use:")
+print("./ct3_to_ntlm.bin " + ct3 + " " + challenge + "\n")
 #./ct3_to_ntlm.bin 2e1e4bf33006ba41 cb8086049ec4736c
 
-print "To crack with hashcat create a file with the following contents:"
-print ct1 + ":" + challenge
-print ct2 + ":" + challenge + "\n"
+print("To crack with hashcat create a file with the following contents:")
+print(ct1 + ":" + challenge)
+print(ct2 + ":" + challenge + "\n")
 
-print "To crack with hashcat:"
-print "./hashcat -m 14000 -a 3 -1 charsets/DES_full.charset --hex-charset hashes.txt ?1?1?1?1?1?1?1?1\n"
-print "To Crack with crack.sh use the following token"
+print("To crack with hashcat:")
+print("./hashcat -m 14000 -a 3 -1 charsets/DES_full.charset --hex-charset hashes.txt ?1?1?1?1?1?1?1?1\n")
+print("To Crack with crack.sh use the following token")
 #$NETLM$1122334455667788$0836F085B124F33895875FB1951905DD2F85252CC731BB25
 if challenge == "1122334455667788":
-  print "NTHASH:" + ntresp
+  print("NTHASH:" + ntresp)
 else:
-  print "$NETLM$" + challenge + "$" + ntresp
+  print("$NETLM$" + challenge + "$" + ntresp)
